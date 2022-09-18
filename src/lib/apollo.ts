daf69a7caa04855a997e23d1f78610dac89b7f6b
Leonardo Lucas <leonardolucas2020@gmail.com>
2022-09-18 19:28:17 -0400

    Projeto Final

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl57d6gv94hbu01ug24dz2t24/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
